0cd2a1d4ff85ac9d3b94e19ff5df45ec59c6444c
unknown <reachfinbarr@gmailm>
2025-10-09 12:18:44 +0000

    initial commit

--- provider.tf
+++ provider.tf
@@ -1,16 +1,4 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name   = "asr-primary-dev"          # Resource group of the storage account
-    storage_account_name  = "okekestorage"            # Your storage account
-    container_name        = "tfstatefile"            # Your storage container
-    key                  = "terraform.tfstate"      # State file name
-    use_azuread_auth     = true
-    use_oidc             = true
-  }
-}
-
-
-terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
